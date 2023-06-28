ddca41ee946f2dea2f151157ac44a818c73a03a2
matsumurashin <134196388+matsumurashin@users.noreply.github.com>
2023-06-28 15:00:51 +0000

    課題2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,23 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.all
   end
 
   def index
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new
+    if @book.save
+    redirect_to book_path(@book.id)
+    else
+    @book = Book.all
+    render :show
+    end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
